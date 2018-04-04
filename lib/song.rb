0959f77f3601b003c45f5943c0ed45eca8ecefad
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 18:44:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
 
   def self.new_from_filename(filename)
       song = self.new
-      song.name = filename.split[1]
+      song.name = filename.split(/\-|\./)[1]
       song.artist_name = filename.split[0]
       song
   end
