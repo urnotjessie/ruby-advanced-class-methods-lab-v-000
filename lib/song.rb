eb2eaca4e92171d1d575c52554ced8bf33992433
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-04 18:48:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
   end
 
   def self.create_from_filename(filename)
-      song = self.new_from_filename
+      song = self.new_from_filename(feilname)
       song.save
       song
   end
